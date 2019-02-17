df2657a5a0c888bdf487bbd244a51a7d7415aa7f
Balakrishna0007 <balakrishna769@gmail.com>
2019-02-17 15:29:01 -0500

    hello

--- bank-application/src/main/java/com/cybrilla/Suma1.java
+++ bank-application/src/main/java/com/cybrilla/Suma1.java
@@ -2,10 +2,8 @@ package com.cybrilla;
 
 public class Suma1 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args)
+	{
 		System.out.println("my own file");
-
 	}
-
 }
